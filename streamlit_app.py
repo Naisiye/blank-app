755e57a0fec4d961404abe45a72f49811d3fd668
Naisiye <naiserian.kyama@strathmore.edu>
2024-10-05 16:09:47 +0000

    I have committed the csv file,modelpkl,app and dtc model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
-import pickle  
+import pickle
 import numpy as np
 import streamlit as st
 
 model = pickle.load(open('model.pkl', 'rb'))
+# Allows for the trained model to be accessed through the file model.pkl
 
 col0, col1, col2, col3, col4, col5, col6 = st.columns(7)
 with col0:
@@ -12,32 +13,32 @@ with col1:
 with col2:
     st.write('')    
 with col3:
-    st.title("ⴍage") 
+    st.title("Iris")
+    # The title looks more centred, just to make it appealing 
 with col4:
     st.write('')
 with col5:
     st.write('')
 with col6:
     st.write('')
+# this guides the app layout
 
 col7, col8, col9 = st.columns(3)
 with col7:
     st.write('')    
 with col8:
-    st.markdown("<h6 style='text-align: center;'>A simple web app to predict annual salary</h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'>Welcome to Iris. A simple web app to predict Iris species</h6>", unsafe_allow_html=True)
+    # Welcome the user to the app.
 with col9:
     st.write('')
 
-gen_list = ["Female", "Male"]
-edu_list = ["Bachelor's", "Master's", "PhD"]
-job_list = ["Director of Marketing", "Director of Operations", "Senior Data Scientist", "Senior Financial Analyst", "Senior Software Engineer"]
-job_idx = [0, 1, 10, 11, 20]
 
-gender = st.radio('Pick your gender', gen_list)
-age = st.slider('Pick your age', 21, 55)
-education = st.selectbox('Pick your education level', edu_list)
-job = st.selectbox('Pick your job title', job_list)
-experience = st.slider('Pick your years of experience', 0.0, 25.0, 0.0, 0.5, "%1f")
+s_length = st.slider('Enter Sepal Length', 4.0, 8.0)
+s_width = st.slider('Enter Sepal Width', 2.0, 5.0)
+p_length = st.slider('Enter Petal Length', 1.0, 7.0)
+p_width = st.slider('Enter Petal Width', 0.1, 3.0)
+
+# Allows the user to input the petal and sepal measurements
 
 col10, col11, col12, col13, col14 = st.columns(5)
 with col10:
@@ -45,25 +46,30 @@ with col10:
 with col11:
     st.write('')    
 with col12:
-    predict_btn = st.button('Predict Salary')
+    predict_btn = st.button('Predict Iris Species')
 with col13:
     st.write('')
 with col14:
     st.write('')
 
+# species_names = ['Iris-setosa','Iris-versicolor','Iris-virginica']
+
 if(predict_btn):
-    inp1 = int(age)
-    inp2 = float(experience)
-    inp3 = int(job_idx[job_list.index(job)])
-    inp4 = int(edu_list.index(education))
-    inp5 = int(gen_list.index(gender))
-    X = [inp1, inp2, inp3, inp4, inp5]
-    salary = model.predict([X])
+    inp1 = float(s_length)
+    inp2 = float(s_width)
+    inp3 = float(p_length)
+    inp4 = float(p_width)
+    
+    # typecast the values to be float type
+    X = [inp1, inp2, inp3, inp4]
+
+    species = model.predict(X)
+# the input values are passed onto the model for prediction
     col15, col16, col17 = st.columns(3)
     with col15:
         st.write('')    
     with col16:
-        st.text(f"Estimated salary: ${int(salary[0])}")
+        st.text(f"Predicted Iris Species: {species[0]}")
+        # displays the species predicted by the model.
     with col17:
         st.write('')
-
